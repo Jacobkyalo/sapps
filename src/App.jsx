708a0acfbb96bb159f3651b806cc3179f244d64d
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-08 19:45:22 +0300

    wrapped UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,24 @@ import Report from "./pages/Report";
 import "./App.css";
 import FormContextProvider from "./contexts/FormContext";
 import AddUnitModal from "./components/AddUnitModal";
+import UserContextProvider from "./contexts/UserContext";
 
 function App() {
   return (
-    <FormContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/add" element={<AddUnitModal />} />
-          <Route path="/report" element={<Report />} />
-        </Routes>
-      </Router>
-    </FormContextProvider>
+    <UserContextProvider>
+      <FormContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard/add" element={<AddUnitModal />} />
+            <Route path="/report" element={<Report />} />
+          </Routes>
+        </Router>
+      </FormContextProvider>
+    </UserContextProvider>
   );
 }
 
