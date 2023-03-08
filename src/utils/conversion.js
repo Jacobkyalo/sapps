ca99739b531fd6909badc7e86889a5c044865f0e
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-08 12:48:39 +0300

    added string capitalize helper fun

--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -4,3 +4,7 @@ export const convertGPA = (score, total) => {
 
   return GPA;
 };
+
+export const capitalizeWords=(string) =>{
+  return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
