62dc985f094b83b4a363f1742e48193d5d34c65b
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-08 10:32:41 +0300

    updated routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import Report from "./pages/Report";
 import "./App.css";
+import FormContextProvider from "./contexts/FormContext";
+import AddUnitModal from "./components/AddUnitModal";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/report" element={<Report />} />
-      </Routes>
-    </Router>
+    <FormContextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/add" element={<AddUnitModal />} />
+          <Route path="/report" element={<Report />} />
+        </Routes>
+      </Router>
+    </FormContextProvider>
   );
 }
 
