855f50af740157465570078203371e568f1d0161
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-08 15:34:09 +0300

    prediction score helper function

--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -1,10 +1,15 @@
 export const convertGPA = (score, total) => {
   const maxGPA = 4;
   const GPA = (Number(score) * maxGPA) / Number(total);
-
   return GPA;
 };
 
-export const capitalizeWords=(string) =>{
-  return string.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+export const capitalizeWords = (string) => {
+  return string.replace(/(?:^|\s)\S/g, function (a) {
+    return a.toUpperCase();
+  });
+};
+
+export const getRandomScore = (min, max) => {
+  return Math.floor(Math.random() * (Number(max) - Number(min))) + Number(min);
 };
